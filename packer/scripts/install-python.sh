987ad4fe37c7c05a6f721a79dc20aac4dd81a6c1
Oyediran Phillip Oyeniyi <oyeniyioyediranphillip@gmail.com>
2026-01-04 15:48:45 +0100

    edit packer code for postgres package

--- packer/scripts/install-python.sh
+++ packer/scripts/install-python.sh
@@ -3,7 +3,7 @@ set -e
 sudo yum update -y
 sudo yum install -y python3 python3-pip git gcc python3-devel
 sudo amazon-linux-extras install postgresql14 -y
-sudo yum install -y postgresql-dev
+sudo yum install -y postgresql-devel
 sudo -u ec2-user python3 -m pip install --upgrade pip
 sudo mkdir -p /home/ec2-user/app
 sudo chown -R ec2-user:ec2-user /home/ec2-user/app
